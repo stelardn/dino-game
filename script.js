2e2abebdbfff434a089b9edb6ef58f109d46d4aa
Stéfany Larissa <stelar.dev@gmail.com>
2022-05-15 20:06:31 -0300

    Obstacle update

--- script.js
+++ script.js
@@ -38,61 +38,60 @@ function play () {
                     }
             }, 20)
         
-            } else { //subindo:
+            } else { // subindo:
                 position += 10;
 
                 DINO.style.bottom = position + 'px';
             }
                 
-        }, 20) // codigo executado a cada 20ms
+        }, 20) // código executado a cada 20ms
 
     }
 
-    //CACTOS:
-
-    function createCactus() {
-        const cactus = document.createElement('div'); // cria uma div no documento html
-        let cactusPosition = 1000; // cacto inicia à direita
+    // OBSTÁCULOS:
+    
+    function createObstacle() {
+        const obstacle = document.createElement('div'); // cria uma div no documento html
+        let obstaclePosition = 1000; // obstáculo inicia à direita
         let randomTime = Math.random() * 4500; // gera um número aleatório entre 0 e 1, que vai ser multiplicado por 4500 para dar um valor em ms
 
-        cactus.classList.add('cactus'); // cria uma classe com o cacto
-        cactus.style.left = 1000 + 'px'; // cacto iniciado 1000px à direita
-        background.appendChild(cactus); // adiciona a div de cacto à div do background
+        obstacle.classList.add('obstacle'); // cria uma classe com o obstáculo
+        obstacle.style.left = 1000 + 'px'; // obstáculo iniciado 1000px à direita
+        background.appendChild(obstacle); // adiciona a div de obstáculo à div do background
 
         var leftInterval = setInterval(() => {
-            if (cactusPosition < -60) { // exclui o elemento criado quando ele sai da tela, evitando processamento desnecessário
+            if (obstaclePosition < -60) { // exclui o elemento criado quando ele sai da tela, evitando processamento desnecessário
                 clearInterval(leftInterval);
-                background.removeChild(cactus);
-            } else if (cactusPosition > 80 && cactusPosition < 140 && position < 60) { 
-                // região de contato entre dinossauro e cacto
+                background.removeChild(obstacle);
+            } else if (obstaclePosition > 80 && obstaclePosition < 140 && position < 60) { 
+                // região de contato entre dinossauro e obstáculo
                 document.body.innerHTML = `<h1 class="game-over">JAVASCRIPT BEATS YOU! 
                 <div>Final Score:</div>${scoreCounter}
                 <div> Press SPACE to play again</div></h1`; 
                 clearInterval(leftInterval);
                 clearInterval(scoreInterval);
-                background.removeChild(cactus);
+                background.removeChild(obstacle);
                 document.addEventListener('keydown', handleReplay); 
             } else {
-                cactusPosition -= 10;
-                cactus.style.left = cactusPosition + 'px'; // faz o cacto se mover para a esquerda enquanto ainda estiver na tela
+                obstaclePosition -= 10;
+                obstacle.style.left = obstaclePosition + 'px'; // faz o obstáculo se mover para a esquerda enquanto ainda estiver na tela
             }
         }, 30)
 
-        setTimeout(createCactus, randomTime); //função JS que invoca uma função em um determinado tempo
+        setTimeout(createObstacle, randomTime); // função JS que invoca uma função em um determinado tempo
     }
-
-    createCactus(); // um cacto é criado imediatamente quando o jogo começa
+    
+    createObstacle(); // um obstáculo é criado imediatamente quando o jogo começa
 
     document.addEventListener('keydown', handleKeyUp); 
 
     var scoreCounter = 0;
     
-
     var scoreInterval = setInterval(() => {
         var SCORE = document.getElementById('score');
         SCORE.innerHTML = `<div class="score"> SCORE <div class="score">${scoreCounter}</div></div>`;
         scoreCounter++;
-    }, 100);
+    }, 100); // Como no jogo original, fazemos a contagem de pontos de acordo com o tempo que o jogador consegue permanecer no jogo
     
 }
 
